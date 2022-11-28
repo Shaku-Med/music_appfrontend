6e92b3e9a37f52d88fb10d5d98c89287f01f7297
Shaku-Med <jujubelt124@gmail.com>
2022-11-28 00:31:04 -0500

    first-commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('mainpage'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
