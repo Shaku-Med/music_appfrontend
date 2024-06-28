4c521a340798115ba5bdffd0dd2eb4e1325f17ed
Magendaam <amaramagenda18@gmail.com>
2024-06-28 03:46:35 -0400

    s

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import { ContextAudio } from './Audconnection/ContextAudio';
 import { HashRouter } from 'react-router-dom';
 import Routing from './Routing';
 
-function App() {
+let App = () => {
 
   const [auth, setauth] = useState(true)
-  const [usr, setusr] = useState('')
+  const [usr, setusr] = useState(null)
 
   // Change max routing...
   const [value, setvalue] =  useState(localStorage.getItem("index"))
@@ -20,25 +20,20 @@ function App() {
   const [audid, setaudid] =  useState(false)
 
   useLayoutEffect(() => { 
-    if(window.top !== window.self){ 
-      setusr(false)
-    }
-    else { 
-      setusr(true)
-      setInterval(() => {
-        if(Cookies.get("c_usr")){ 
-          if(Cookies.get("c_usr") !== null){ 
-            setauth(true)
-          }
-          else { 
-            setauth(false)
-          }
+    setusr(true)
+    setInterval(() => {
+      if(Cookies.get("c_usr")){ 
+        if(Cookies.get("c_usr") !== null){ 
+          setauth(true)
         }
         else { 
           setauth(false)
         }
-      }, 10);
-    }
+      }
+      else { 
+        setauth(false)
+      }
+    }, 10);
 
     var a;
 
@@ -82,46 +77,52 @@ function App() {
 
   return (
     <>
-     { 
-       [usr].map((v, k) => { 
-        if(v === true){ 
-          return ( 
-            <div key={k}>
-            { 
-              [auth].map((val, key) => { 
-                if(val !== true){ 
-                  return ( 
-                    <Auth key={key}/>
+      {
+        usr && (
+          <>
+            {
+              [usr].map((v, k) => {
+                if (v === true) {
+                  return (
+                    <div key={k}>
+                      {
+                        [auth].map((val, key) => {
+                          if (val !== true) {
+                            return (
+                              <Auth key={key} />
+                            )
+                          }
+                          else {
+                            return (
+                              <HashRouter key={key}>
+                                <ContextAudio.Provider value={{ value, setvalue, playing, setplaying, similar, setplaysimilar, audid, setaudid }}>
+                                  <Audio />
+                                  <Routing />
+                                </ContextAudio.Provider>
+                              </HashRouter>
+                            )
+                          }
+                        })
+                      }
+                    </div>
                   )
                 }
-                else { 
-                  return ( 
-                      <HashRouter key={key}>
-                         <ContextAudio.Provider value={{value, setvalue, playing, setplaying, similar, setplaysimilar, audid, setaudid}}>
-                            <Audio/>
-                            <Routing/>
-                         </ContextAudio.Provider>
-                      </HashRouter>
+                else {
+                  console.clear()
+                  return (
+                    <div key={k}>
+                      <div className="d-flex justify-content-center align-items-center " style={{ flexDirection: 'column', height: '100vh' }} >
+                        <img src="https://i.pinimg.com/originals/46/10/95/46109566f97a41cb76393d9d668e6ff3.gif" style={{ height: "10rem" }} alt="" className="text-center" />
+                        <div className="h1 font-bold d-flex justify-content-center align-items-center p-4 font-weight-bold">UnAuthorize Route.</div>
+                      </div>
+                    </div>
                   )
                 }
               })
             }
-          </div>
-          )
-        }
-        else { 
-          console.clear()
-          return ( 
-            <div key={k}>
-              <div className="d-flex justify-content-center align-items-center " style={{flexDirection: 'column', height: '100vh'}} >
-              <img   src="https://i.pinimg.com/originals/46/10/95/46109566f97a41cb76393d9d668e6ff3.gif" style={{height: "10rem"}} alt="" className="text-center"/>
-              <div className="h1 font-bold d-flex justify-content-center align-items-center p-4 font-weight-bold">UnAuthorize Route.</div>
-              </div>
-            </div>
-          )
-        }
-       })
-     }
+          </>
+        )
+      }
     </>
   );
 }
